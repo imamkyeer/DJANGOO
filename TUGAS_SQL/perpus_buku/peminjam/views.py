1cbdae1d4141dbb36263e76fdb43132234857dc5
imam surya <sorrybos79@gmail.com>
2026-06-17 10:08:36 +0700

    sasa

--- TUGAS_SQL/perpus_buku/peminjam/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import perpus_db
-from django.http import HttpResponse
-
-def index(request):
-    return render("Halo Perpustakaan")
